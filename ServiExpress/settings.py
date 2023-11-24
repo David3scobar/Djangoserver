900b2188d1bea4135b5968525bf09a74dfcfff38
David3scobar <118226892+David3scobar@users.noreply.github.com>
2023-11-24 16:50:33 -0300

    Update settings.py
    allowed host

--- ServiExpress/settings.py
+++ ServiExpress/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=ke)n2b%v1!lme%6c!pjpbx-@x)xd4o=c^p(o*u##7&rjo*ek6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.207.168.250']
 
 
 # Application definition
